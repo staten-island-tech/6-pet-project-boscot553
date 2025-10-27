bd96a7aa40c764d68170976ae90dd338f0f3ddd1
boscot553 <yizhengtian2011@gmail.com>
2025-10-27 10:16:13 -0400

    Update class.py

--- class.py
+++ class.py
@@ -1,3 +1,32 @@
-import pickle
-             
-pickle.foward(10)
+import random
+sick = random.randint(0, 1)
+class pet():
+    def __init__(self, name, food, water):
+        self.name = name
+        self.food = food
+        self.water = water
+    def addfood(self, food):
+        self.food += food
+        print(self.food)
+    def addwater(self, water):
+        self.water += water
+        print(self.water)
+    def health(self):
+        if sick < 1:
+            print("Healthy")
+        else:
+             print("Sick")
+
+
+
+        
+
+dog = pet("dog", 10, 0)
+dog.health()
+
+
+
+
+
+        
+
